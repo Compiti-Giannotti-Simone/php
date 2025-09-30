a149824088e89382db7af41cb5c692c377499e77
simzer0 <smngiannotti@gmail.com>
2025-09-30 08:25:04 +0200

    add dice assignment (es3)

---
+++ src/es3/index.php
@@ -0,0 +1,52 @@
+<?php
+function draw_die($number)
+{
+    $pips = [
+        1 => [5],
+        2 => [1, 9],
+        3 => [1, 5, 9],
+        4 => [1, 3, 7, 9],
+        5 => [1, 3, 5, 7, 9],
+        6 => [1, 3, 4, 6, 7, 9]
+    ];
+    $html = '<div class="die">';
+    for ($i = 1; $i <= 9; $i++) {
+        $html .= '<span class="pip' . (in_array($i, $pips[$number]) ? ' on' : '') . '"></span>';
+    }
+    $html .= '</div>';
+    return $html;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+
+<body>
+    <form class="text-center mt-5" action="" method="get">
+        <button type="submit" class="btn btn-primary">Roll</button>
+    </form>
+
+    <?php
+    $die1 = rand(1, 6);
+    $die2 = rand(1, 6);
+    echo '<div class="text-center mt-4 d-flex justify-content-center gap-4">';
+    echo draw_die($die1);
+    echo draw_die($die2);
+    echo '</div>';
+    ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
